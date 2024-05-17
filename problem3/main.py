ee119715e26f70d24e4c7b3544908c5c13721490
agusfn15 <nagusfernando@gmail.com>
2024-05-18 02:08:16 +0700

    solve problem 3

--- problem3/main.py
+++ problem3/main.py
@@ -1,5 +1,13 @@
 def fibonacci(number):
-    return 0
+    if number <= 0:
+        return 0
+    elif number == 1:
+        return 1
+    else:
+        fib = [0, 1]
+        for i in range(2, number + 1):
+            fib.append(fib[i - 1] + fib[i - 2])
+        return fib[number]
 
 if __name__ == "__main__":
     print(fibonacci(0))  # 0
